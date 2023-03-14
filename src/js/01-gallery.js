55cb45c84f43ecd4d381c06104462ff53b12908f
OIgaB <linkbolya@gmail.com>
2023-03-15 01:54:38 +0200

    Complete the 1st & 2nd task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+"use strict";
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+// 1. Створюємо розмітку і додаємо її в DOM
+const galleryEl = document.querySelector('.gallery'); //<ul>
+galleryEl.style.listStyle = 'none';
+
+const elements = galleryItems.map(({ preview, original, description } = {}) =>
+    `<li 
+        <a class="gallery__item" href=${original}>
+            <img 
+                class="gallery__image"
+                src=${preview} 
+                alt=${description}
+            />
+        </a>
+    </li>
+    `).join('');
+
+galleryEl.innerHTML = elements;
+console.log(galleryEl);
+
+
+// 2. Створюємо модальне вікно - галерею (з підписами зображень) через бібліотеку basicLightbox
+new SimpleLightbox('.gallery .gallery__item', { 
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,  
+});
